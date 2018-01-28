d3415b43ebe01c1e4eae94f7cf5120e399335230
Roger Hu <rogermylifeQ@gmail.com>
2018-01-28 23:49:46 +0800

    2018/1/28

---
+++ DoNotStarveTogether/Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,39 @@
+return {
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-358749986"]={
+    configuration_options={ IndicatorSize=3, MaxIndicator=7000, PlayerIndicators=1 },
+    enabled=true 
+  },
+  ["workshop-369596587"]={ configuration_options={ message_style=1 }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-786556008"]={
+    configuration_options={ ENABLEBACKPACK=false, EXTRASLOT=0, INVENTORYSIZE=45 },
+    enabled=true 
+  } 
+}
